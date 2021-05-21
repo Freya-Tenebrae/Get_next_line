a7b4518194193ce0b64aa083f525c84f6839decc
Freya-Tenebrae <freya.tenebrae@gmail.com>
2021-05-21 02:04:08 +0200

    getnextline v1

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,70 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 00:14:30 by cmaginot          #+#    #+#             */
-/*   Updated: 2021/04/24 00:14:45 by cmaginot         ###   ########.fr       */
+/*   Updated: 2021/05/21 02:03:44 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int get_next_line(int fd, char **line)
-{
+#include "get_next_line.h"
 
+static int	ft_is_fd_correct(int fd, char **line, char **str)
+{
+	if (fd == -1 || line == 0 || BUFFER_SIZE <= 0)
+		return (-1);
+	*str = malloc (sizeof(char) * (BUFFER_SIZE + 1));
+	if (!*str)
+		return (-1);
+	return (0);
+}
+
+static char	*ft_read_line(int fd, char *str)
+{
+	char	buf[BUFFER_SIZE + 1];
+	int		pos;
+
+	pos = 1;
+	while (pos > 0)
+	{
+		pos = read(fd, buf, BUFFER_SIZE);
+		buf[pos] = '\0';
+		str = ft_strjoin(str, buf);
+	}
+	return (str);
+}
+
+static int	ft_put_in_line(char **line, char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0')
+		return (0);
+	else
+	{
+		while (str[i] != '\n' || str[i] != '\0')
+			i++;
+		if (i == 0)
+			*line = ft_strdup("");
+		else
+		{
+			*line = ft_strdup(str);
+		}
+		return (1);
+	}
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*str;
+	int			result;
+
+	if (ft_is_fd_correct(fd, line, &str) != 0)
+		return (-1);
+	else
+	{
+		str = ft_read_line(fd, str);
+		result = ft_put_in_line(line, str);
+		free(str);
+		return (result);
+	}
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,6 +6,76 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 00:14:30 by cmaginot          #+#    #+#             */
-/*   Updated: 2021/04/24 00:15:50 by cmaginot         ###   ########.fr       */
+/*   Updated: 2021/05/21 01:44:36 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*str;
+	size_t			i;
+
+	str = (unsigned char *)b;
+	i = -1;
+	while (++i < len)
+		*str++ = (unsigned char)c;
+	return (b);
+}
+
+static void	ft_bzero(void *b, size_t n)
+{
+	ft_memset(b, 0, n);
+}
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*res;
+	size_t			len;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (s1 == 0 || s2 == 0)
+		return (0);
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	res = malloc(len * sizeof(char));
+	if (!res)
+		return (0);
+	ft_bzero(res, len);
+	i = -1;
+	j = 0;
+	while (s1[++i] != '\0')
+		res[i] = s1[i];
+	while (s2[j] != '\0')
+		res[i++] = s2[j++];
+	res[i] = '\0';
+	return (res);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*str;
+	size_t	i;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	str = malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (0);
+	i = -1;
+	while (++i < len)
+		str[i] = s1[i];
+	str[i] = '\0';
+	return (str);
+}
