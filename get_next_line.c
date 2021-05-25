e90237d80e4e94cb883dd43a4cbcc044c5114899
Freya-Tenebrae <freya.tenebrae@gmail.com>
2021-05-25 13:41:31 +0200

    getnextlinev8

--- get_next_line.c
+++ get_next_line.c
@@ -6,74 +6,56 @@
 /*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/24 00:14:30 by cmaginot          #+#    #+#             */
-/*   Updated: 2021/05/24 14:24:51 by cmaginot         ###   ########.fr       */
+/*   Updated: 2021/05/25 13:41:04 by cmaginot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_is_fd_correct(int fd, char **line, char **str)
+static void    ft_read_line(int fd, char **str)
 {
-	if (fd <= -1 || line == 0 || BUFFER_SIZE <= 0)
-		return (-1);
-	*str = malloc (sizeof(char) * (BUFFER_SIZE + 1));
-	if (!*str)
-		return (-1);
-	return (0);
+    char    *buf;
+    int        i;
+
+    buf = malloc (sizeof(char) * (BUFFER_SIZE + 1));
+    if (!*buf)
+        return ;
+    i = read(fd, buf, BUFFER_SIZE);
+    while (i > 0)
+    {
+        buf[i] = '\0';
+        *str = ft_strjoin(*str, buf);
+        i = read(fd, buf, BUFFER_SIZE);
+    }
+    free(buf);
 }
 
-static char	*ft_read_line(int fd, char *str)
+static void    ft_put_in_line(char ***line, char *str)
 {
-	char	buf[BUFFER_SIZE + 1];
-	int		i;
-	int		j;
-
-	i = read(fd, buf, BUFFER_SIZE);
-	j = 0;
-	while (i > 0)
-	{
-		buf[i] = '\0';
-		str = ft_strjoin(str, buf);
-		i = i + read(fd, buf, BUFFER_SIZE);
-	}
-	return (str);
+    if (str[0] == '\n' || str[0] == '\0')
+        **line = ft_strdup("");
+    else
+        **line = ft_strdup(str);
 }
 
-/*
-	fonction todo correctly
-*/
-
-static char	*ft_put_in_line(char *str)
+int    get_next_line(int fd, char **line)
 {
-	int		i;
-	char	*res;
+    static char    *str;
 
-	i = 0;
-	while (str[i] != '\n' || str[i] != '\0')
-		i++;
-	if (i == 0)
-		res = ft_strdup("");
-	else
-		res = ft_strdup(str);
-	return (res);
-}
-
-int	get_next_line(int fd, char **line)
-{
-	static char	*str;
-
-	if (ft_is_fd_correct(fd, line, &str) != 0)
-		return (-1);
-	else
-	{
-		str = ft_read_line(fd, str);
-		if (*str)
-		{
-			*line = ft_put_in_line(str);
-			if (!*line)
-				return (-1);
-			return (1);
-		}
-		return (0);
-	}
+    if (line || fd <= -1 || BUFFER_SIZE <= 0)
+        return (-1);
+    str = malloc (sizeof(char) * (BUFFER_SIZE + 1));
+    if (!str)
+        return (-1);
+    ft_read_line(fd, &str);
+    if (str)
+    {
+        ft_put_in_line(&line, str);
+        free(str);
+        if (!line)
+            return (-1);
+        return (1);
+    }
+    free(str);
+    return (0);
 }
