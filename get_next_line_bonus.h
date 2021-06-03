175dcaf811b267382c9b5379bec24d6f483d02f8
Freya-Tenebrae <34307281+Freya-Tenebrae@users.noreply.github.com>
2021-06-03 14:00:13 +0200

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmaginot <cmaginot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/24 00:14:30 by cmaginot          #+#    #+#             */
+/*   Updated: 2021/06/03 13:45:02 by cmaginot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+/* ************************************************************************** */
+/*                                  INCLUDES                                  */
+/* ************************************************************************** */
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+/* ************************************************************************** */
+/*                                  FUNCTION                                  */
+/* ************************************************************************** */
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_strndup(const char *s1, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
